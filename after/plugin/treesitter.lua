a2e9fb4e960b85c5300638c83937cd4ab62ee37f
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-12-25 20:44:02 +0100

    UPDATE - treesinter config file format

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,13 +1,10 @@
-local status, treesitter = pcall(require,
-    "nvim-treesitter.configs")
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-
     return
 end
 
-treesitter. setup ({
-    highlight = {
-        enable = true},
+treesitter.setup({
+    highlight = { enable = true },
     indent = { enable = true },
     autotag = { enable = true },
     ensure_installed = {
@@ -23,14 +20,11 @@ treesitter. setup ({
         "svelte",
         "rust",
         "java",
-
         "bash",
         "lua",
         "vim",
         "dockerfile",
         "gitignore",
-
     },
     auto_install = true,
-
 })
